beb66d52eb0962ccf479a9200be814ef6071312b
ahmed <mido1esam2@gmail.com>
2025-04-12 14:03:01 +0200

    Invert-Values

---
+++ Codewars/Invert-Values.js
@@ -0,0 +1,9 @@
+/*
+Given a set of numbers, return the additive inverse of each. 
+Each positive becomes negatives, and the negatives become positives.
+*/
+
+function invert(array) {
+  return array.map((num) => num * -1);
+}
+console.log(invert([1, -2, 3, -4, 5]));
